5eabaf7cf0160ebde617479916074d23fcbe5294
Harsh_master <harshsinghmarwah@gmail.com>
2020-10-30 22:15:08 +0530

    HARSH COMMIT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,53 +1,53 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import styles from './home.module.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
 
 const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles["dropdowns"]}>
+        <div className={styles.dropdown}>
+          <h5>Select semester:</h5>
+          <select>
+            <option value="1">1</option>
+          </select>
+        </div>
+        <div className={styles.dropdown}>
+          <h5>Select course:</h5>
+          <select>
+            <option value="DBMS">DBMS</option>
+          </select>
+        </div>
+        <div className={styles.dropdown}>
+          <h5>Select exam:</h5>
+          <select>
+            <option value="CAT-1">CAT-1</option>
+          </select>
+        </div>
+        <div className={styles.dropdown}>
+          <h5>Select faculty:</h5>
+          <select>
+            <option value="Vellengiri J">Vellengiri J</option>
+          </select>
+        </div>
+      </div>
+      <Link
+        to={{
+          pathname: "/Test",
+          state: {
+            selectedOptions: {
+              semester: 1,
+              course: "DBMS",
+              faculty: "Vellengiri J",
+              exam: "CAT-1",
+            },
+          },
+        }}
+      >
+        <button className={styles.submit}>Submit</button>
+      </Link>
+    </div>
+  );
+};
 
-
-    return (
-        <div className={styles.container}>
-            <div className={styles['dropdowns']}>
-                <div className={styles.dropdown}>
-                    <h5>Select semester:</h5>
-                    <select >
-                        <option value="1">1</option>
-                    </select>
-                </div>
-                <div className={styles.dropdown}>
-                    <h5>Select course:</h5>
-                    <select >
-                        <option value="1">dummy_course_1</option>
-                    </select>
-                </div>
-                <div className={styles.dropdown}>
-                    <h5>Select exam:</h5>
-                    <select >
-                        <option value="1">dummy_exam_1</option>
-                    </select>
-                </div>
-                <div className={styles.dropdown}>
-                    <h5>Select faculty:</h5>
-                    <select >
-                        <option value="1">dummy_faculty_1</option>
-                        <option value="2">dummy_faculty_1</option>
-                    </select>
-                </div>
-            </div>
-            <Link to={{
-                pathname: '/Test',
-                state: {
-                    selectedOptions: {
-                        "semester": 1,
-                        "course": "dummy_course_1",
-                        "faculty": "dummy_faculty_1",
-                        "exam": "dummy_exam_1"
-                    }
-
-                }
-            }}><button className={styles.submit}>Submit</button></Link >
-        </div >
-    )
-}
-
-export default Home
+export default Home;
--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,136 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import styles from './test.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./test.module.css";
 
 const Test = (props) => {
-    const [questionList, setquestionList] = useState()
-    const [Submited, setSubmited] = useState(false)
-    const [score, setScore] = useState(0)
-    const [selectedQuestion, setSelectedQuestion] = useState(0)
-    useEffect(() => {
-        console.log(props.location.state)
+  const [questionList, setquestionList] = useState();
+  const [Submited, setSubmited] = useState(false);
+  const [score, setScore] = useState(0);
+  const [selectedQuestion, setSelectedQuestion] = useState(0);
+  useEffect(() => {
+    console.log(props.location.state);
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        var raw = JSON.stringify(props.location.state.selectedOptions);
+    var raw = JSON.stringify(props.location.state.selectedOptions);
 
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'manual'
-        };
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "manual",
+    };
 
-        fetch("https://vit-vellore.herokuapp.com/student/getQuestions/getAllQuestions", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                setquestionList(result.questions)
-                console.log(result)
-            })
-            .then(() => {
-                questionList.map(question => {
-                    question.selectedOption = ""
-                    question.remark = ""
-                    question.score = 0
-                })
-            })
-            .catch(error => console.log('error', error));
+    fetch(
+      "https://vit-vellore.herokuapp.com/student/getQuestions/getAllQuestions",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setquestionList(result.questions);
+        console.log(result);
+      })
+      .then(() => {
+        // questionList.map((question) => {
+        //   question.selectedOption = "C";
+        //   question.remark = "";
+        // });
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
 
-
-    }, [])
-
-    const submitResponse = () => {
-        setSubmited(true)
-        var totalScore = 0
-        for (var i = 0; i < questionList.length; i++) {
-            if (questionList[i].correct_option == questionList[i].selectedOption) {
-                totalScore += 4
-                questionList[i].score = 4
-            } else {
-                totalScore += 0
-                questionList[i].score = 0
-            }
-        }
-        setScore(totalScore)
-
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        var raw = JSON.stringify({
-            "email": localStorage.getItem("user"),
-            "responses": questionList
-        });
-
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'manual'
-        };
-
-        fetch("https://vit-vellore.herokuapp.com/student/storeResponses", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                console.log(result)
-            })
-            .catch(error => console.log('error', error));
+  const submitResponse = () => {
+    setSubmited(true);
+    var totalScore = 0;
+    for (var i = 0; i < questionList.length; i++) {
+      questionList[i].selected_option = "C";
+      console.log(questionList[i]);
     }
 
-    return (
-        <div className={styles.container}>
-            {Submited &&
-                <div>
-                    <h2>Response Submitted.</h2>
-                    <p style={{ textAlign: 'center' }}>Your score is <b>{score}</b></p>
-                </div>
-            }
-            {questionList && !Submited &&
-                <div>
+    setScore(totalScore);
 
-                    <div className={styles.header}>
-                        <h2>Questions</h2>
-                        <div className={styles['question-list']}>
-                            {
-                                questionList.map((question, index) => {
-                                    return (
-                                        <button style={{ backgroundColor: selectedQuestion == index ? "rgb(94, 164, 255)" : null }} onClick={() => setSelectedQuestion(index)}>{index + 1}</button>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-                    <div className={styles.question}>
-                        <h1>Question {selectedQuestion + 1}</h1>
-                        <p className={styles['question-text']}>{questionList[selectedQuestion].ques_text}</p>
-                        <div className={styles.options}>
-                            {questionList[selectedQuestion].options.map(option => {
-                                return (
-                                    <div className={styles.option}>
-                                        <div className={styles.radio} style={{ backgroundColor: questionList[selectedQuestion].selectedOption == option ? 'rgb(94, 164, 255)' : "white" }}
-                                            onClick={() => {
-                                                var tempArr = questionList.slice(0)
-                                                tempArr[selectedQuestion].selectedOption = option
-                                                setquestionList(tempArr)
-                                            }} />
-                                        <p>{option}</p>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <div className={styles.remark}>
-                            <h5>Remark:</h5><textarea onChange={(e) => {
-                                var tempArr = questionList.slice(0)
-                                tempArr[selectedQuestion].remark = e.target.value
-                                setquestionList(tempArr)
-                            }} value={questionList[selectedQuestion].remark ? questionList[selectedQuestion].remark : ""}></textarea></div>
-                    </div>
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-                    <button onClick={submitResponse} className={styles.submit}>Submit</button>
-                </div>
-            }
-        </div>
+    var raw = JSON.stringify({
+      email: localStorage.getItem("user"),
+      responses: questionList,
+    });
 
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "manual",
+    };
+
+    fetch(
+      "https://vit-vellore.herokuapp.com/student/storeResponses",
+      requestOptions
     )
-}
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
-export default Test
+  return (
+    <div className={styles.container}>
+      {Submited && (
+        <div>
+          <h2>Submitted Successfully</h2>
+        </div>
+      )}
+      {questionList && !Submited && (
+        <div>
+          <div className={styles.header}>
+            <h2>Questions</h2>
+            <div className={styles["question-list"]}>
+              {questionList.map((question, index) => {
+                return (
+                  <button
+                    style={{
+                      backgroundColor:
+                        selectedQuestion == index ? "rgb(94, 164, 255)" : null,
+                    }}
+                    onClick={() => setSelectedQuestion(index)}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.question}>
+            <h1>Question {selectedQuestion + 1}</h1>
+            <p className={styles["question-text"]}>
+              {questionList[selectedQuestion].ques_text}
+            </p>
+            <div className={styles.options}>
+              {questionList[selectedQuestion].options.map((option) => {
+                return (
+                  <div className={styles.option}>
+                    <p>{option}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <h4>Correct Option: A</h4>
+            <h4>Selected Option: C</h4>
+            <div className={styles.remark}>
+              <h5>Remark:</h5>
+              <textarea
+                onChange={(e) => {
+                  var tempArr = questionList.slice(0);
+                  tempArr[selectedQuestion].remark = e.target.value;
+                  setquestionList(tempArr);
+                }}
+                value={
+                  questionList[selectedQuestion].remark
+                    ? questionList[selectedQuestion].remark
+                    : ""
+                }
+              ></textarea>
+            </div>
+          </div>
+
+          <button onClick={submitResponse} className={styles.submit}>
+            Submit
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Test;
